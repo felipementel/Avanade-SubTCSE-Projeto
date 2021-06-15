5a34e4ee5ed5016cc63fb714547b01e23f674f3f
Felipe Augusto <felipementel@hotmail.com>
2021-06-15 15:25:59 -0300

    aula 3

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,20 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public string Id { get; init; }
+
+        public string RoleName { get; init; }
+    }
+}
--- src/Avanade.SubTCSE.Projeto.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Avanade.SubTCSE.Projeto.Domain
-{
-    public class Class1
-    {
-    }
-}
