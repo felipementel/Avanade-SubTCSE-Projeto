4662278acf0223ba19f5ad260362171ad33c7295
Felipe Augusto <felipementel@hotmail.com>
2021-06-17 08:59:21 -0300

    aula 4

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -0,0 +1,7 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
+{
+    public interface IEmployeeRoleService
+    {
+        void AddEmployeeRole(Entities.EmployeeRole employeeRole);
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
+{
+    class EmployeeRoleService
+    {
+    }
+}
