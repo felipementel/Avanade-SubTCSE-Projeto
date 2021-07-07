746c0fb578af029cfc3617c10490b196622c678b
Felipe Augusto <felipementel@hotmail.com>
2021-07-07 08:48:21 -0300

    BaseRepository

---
+++ src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates;
+using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base
+{
+    public abstract class BaseRepository<TEntity, Tid>
+        : IBaseRepository<TEntity, Tid> where TEntity : BaseEntity<Tid>
+    {
+        private readonly IMongoCollection<TEntity> _collection;
+
+        public virtual async Task<TEntity> Add(TEntity entity)
+        {
+            await _collection.InsertOneAsync(entity);
+
+            return entity;
+        }
+
+        public async Task<TEntity> FindById(Tid Id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/EmployeeRole/EmployeeRoleRespository.cs
+++ src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/EmployeeRole/EmployeeRoleRespository.cs
@@ -1,6 +1,12 @@
-﻿namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.EmployeeRole
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository;
+using Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base;
+
+namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.EmployeeRole
 {
     public class EmployeeRoleRespository
+        : BaseRepository<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, string>
+        , IEmployeeRoleRepository
     {
+
     }
 }
